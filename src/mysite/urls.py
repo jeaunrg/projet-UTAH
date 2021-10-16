45784ef4adc25058f136ed512e6735488422b29b
jeaunrg <jeanunrug@gmail.com>
2021-10-16 03:57:11 +0200

    login at entry and add link to administration

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -39,20 +39,12 @@ urlpatterns = [
 	path('must_authenticate/', must_authenticate_view, name="must_authenticate"),
 
     # Password reset links (ref: https://github.com/django/django/blob/master/django/contrib/auth/views.py)
-    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),
-        name='password_change_done'),
-
-    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'),
-        name='password_change'),
-
-    path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_done.html'),
-     name='password_reset_done'),
-
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), name='password_change_done'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'), name='password_change'),
+    path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
-     name='password_reset_complete'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
--- src/personal/views.py
+++ src/personal/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from operator import attrgetter
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
@@ -6,6 +6,9 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 
 def home_screen_view(request, *args, **kwargs):
+
+	if not request.user.is_authenticated:
+		return redirect("login")
 
 	context = {}
 
